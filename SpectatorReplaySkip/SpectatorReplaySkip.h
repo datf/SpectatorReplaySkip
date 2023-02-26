1e50e580af82f62ef52a5a033777b6eef4221f36
datf <github@datf.net>
2023-02-26 13:01:59 +0100

    Plugin implementation
    Skipping replays when spectating an online match

--- SpectatorReplaySkip/SpectatorReplaySkip.h
+++ SpectatorReplaySkip/SpectatorReplaySkip.h
@@ -8,38 +8,10 @@
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
 
-class SpectatorReplaySkip: public BakkesMod::Plugin::BakkesModPlugin/*, public BakkesMod::Plugin::PluginSettingsWindow*//*, public BakkesMod::Plugin::PluginWindow*/
+class SpectatorReplaySkip: public BakkesMod::Plugin::BakkesModPlugin
 {
-
-	//std::shared_ptr<bool> enabled;
-
-	//Boilerplate
 	virtual void onLoad();
 	virtual void onUnload();
-
-	// Inherited via PluginSettingsWindow
-	/*
-	void RenderSettings() override;
-	std::string GetPluginName() override;
-	void SetImGuiContext(uintptr_t ctx) override;
-	*/
-
-	// Inherited via PluginWindow
-	/*
-
-	bool isWindowOpen_ = false;
-	bool isMinimized_ = false;
-	std::string menuTitle_ = "SpectatorReplaySkip";
-
-	virtual void Render() override;
-	virtual std::string GetMenuName() override;
-	virtual std::string GetMenuTitle() override;
-	virtual void SetImGuiContext(uintptr_t ctx) override;
-	virtual bool ShouldBlockInput() override;
-	virtual bool IsActiveOverlay() override;
-	virtual void OnOpen() override;
-	virtual void OnClose() override;
-	
-	*/
+	void Skip();
 };
 
